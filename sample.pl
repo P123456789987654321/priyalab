db97c551832f82f5d9470e85bc71197ed96f392a
P123456789987654321 <144648322+P123456789987654321@users.noreply.github.com>
2024-03-05 22:22:14 -0800

    Add files via upload

---
+++ sample.pl
@@ -0,0 +1,13 @@
+$str1="Hello World";
+$str2="@ Welcome BCA @";
+print($str1,"\n");
+print($str2,"\n");
+print(length($str1),"\n");
+print(length($str2),"\n");
+print(uc($str1),"\n");
+print(lc($str1),"\n");
+print(uc($str2),"\n");
+print(lc($str2),"\n");
+$sub="BCA";
+$find=index($str2,$sub);
+print("the string $str2 in position $sub in a string:$find");
